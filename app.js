d7c9d9a3a91e042f921e3eadd2f224718b30e526
buathi_q <buathi_q@epitech.eu>
2014-07-25 17:54:16 +0200

    Add loading queues from redis

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('superagent');
 var cluster = require('cluster');
+var redis = require('redis');
+var url = require('url');
 
 // Load configuration and initialize server
 var config = require('./config/configuration.js');
@@ -11,32 +13,52 @@ var config = require('./config/configuration.js');
 var app = express();
 app.use(bodyParser());
 
-var confQueues = [
-  {
-    name: "dropbox",
-    prefix: "http://localhost:8000",
-    port: config.port + 1
-  },
-  {
-    name: "gmail",
-    prefix: "http://localhost:5000",
-    port: config.port + 2
-  }
-];
+var components = url.parse(config.redisUrl);
 
-confQueues.forEach(function(confQueue) {
-  cluster.fork({
-    QUEUE_PREFIX: confQueue.prefix,
-    QUEUE_NAME: confQueue.name,
-    QUEUE_PORT: confQueue.port
+var client = redis.createClient(components.port || 6379, components.hostname || "localhost", {
+  auth_pass: (components.auth) ? ((components.auth.split(':').length > 1) ? components.auth.split(':')[1] : components.auth) : undefined
+});
+
+client.on('error', function(err) {
+  console.warn('ERROR:', err);
+});
+
+client.keys("*:ids", function (err, keys) {
+  if(err) {
+    return console.warn('ERROR:', err);
+  }
+
+  var confQueues = [];
+  var port = config.port + 1;
+
+  keys.forEach(function(key) {
+    key = key.replace(':ids', '');
+
+    var matches = key.match(/^https?:\/\/([a-zA-Z0-9]+)/);
+
+    confQueues.push({
+      name: matches[1],
+      prefix: key,
+      port: port
+    });
+
+    port += 1;
   });
 
-  app.use('/' + confQueue.name, function(req, res) {
-    if(req.url === "/") {
-      req.url = "/active";
-    }
+  confQueues.forEach(function(confQueue) {
+    cluster.fork({
+      QUEUE_PREFIX: confQueue.prefix,
+      QUEUE_NAME: confQueue.name,
+      QUEUE_PORT: confQueue.port
+    });
 
-    req.pipe(request("http://localhost:" + confQueue.port + req.url)).pipe(res);
+    app.use('/' + confQueue.name, function(req, res) {
+      if(req.url === "/") {
+        req.url = "/active";
+      }
+
+      req.pipe(request("http://localhost:" + confQueue.port + req.url)).pipe(res);
+    });
   });
 });
 
