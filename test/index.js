78503435a81fcc4494977e1d250298fc7178ea97
buathi_q <buathi_q@epitech.eu>
2014-07-28 15:09:26 +0200

    Update tests

--- test/index.js
+++ test/index.js
@@ -1,25 +1,11 @@
 'use strict';
 
-var config = require("../config/configuration.js");
-var cluster = require('cluster');
+require('should');
 
-describe('Server', function() {
-  it('should launch server', function(done) {
-    if(cluster.isMaster) {
-      var master = require('../app.js');
-
-      // Start the server
-      master.listen(config.port, function() {
-        console.log("Server listening on port " + config.port);
-        done();
-      });
-    }
-    else {
-      var slave = require('../lib/slave.js');
-
-      slave.listen(config.port, function() {
-        console.log("Server `" + process.env.QUEUE_NAME + "` listening on port " + config.port);
-      });
-    }
+describe('app.js', function() {
+  it("shoud return a valid express application", function(done) {
+    var app = require('../app.js');
+    app.should.have.property('listen');
+    done();
   });
 });
